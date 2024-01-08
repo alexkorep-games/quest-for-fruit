d48e4d7765dc86d129d6fe1a1f69ee13e6ccdc81
Alexey Korepanov <alexkorep@gmail.com>
2024-01-08 23:15:49 +0100

    Add teleport

---
+++ src/scenes/teleport/teleport.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("Rotate")
+
+
+func _on_Area2D_body_entered(body):
+	var tween = Tween.new() # Create a new Tween
+	self.add_child(tween) # Add it to the scene tree
+
+	# Set up the Tween to move the body to (0, 0) over 1 second
+	tween.interpolate_property(body, "position", body.position, global_position, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+
+	# Set up the Tween to scale the body down to (0, 0) over 1 second
+	tween.interpolate_property(body, "scale", body.scale, Vector2(0, 0), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+
+	tween.start() # Start the Tween
+	
+	tween.connect("tween_all_completed", self, "_on_Tween_tween_all_completed")
+
+func _on_Tween_tween_all_completed():
+	get_tree().change_scene("res://scenes/start_screen/start_screen.tscn")
