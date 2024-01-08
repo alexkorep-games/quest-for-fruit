b0dadd7fd81bad6cec3f5f4987406b3ce2493ed0
Alexey Korepanov <alexkorep@gmail.com>
2024-01-08 18:36:24 +0100

    Add a map and the joystick

--- src/scenes/hedgehog/hedgehog.gd
+++ src/scenes/hedgehog/hedgehog.gd
@@ -4,8 +4,19 @@ signal left_screen
 
 # Define your gravity, jump strength, and movement speed
 var gravity = 1200
-var jump_strength = 700
-var move_speed = 40
+var jump_strength = 500
+var move_speed = 150
+var friction = 0.9
+# The speed considered the player is not moving
+var stop_speed = 5
+
+enum State {
+	RUNNING,
+	SITTING,
+	JUMPING
+}
+
+var state = State.SITTING
 
 # This variable will hold the velocity of the KinematicBody2D
 var velocity = Vector2()
@@ -19,47 +30,85 @@ var double_click_delay = 0.5
 var is_mouse_pressed = false
 
 func _physics_process(delta):
-	# Apply gravity
-	velocity.y += gravity * delta
-
-	# Check for a mouse click and if the character is on the ground
-	if Input.is_mouse_button_pressed(BUTTON_LEFT) and is_on_floor() and not is_mouse_pressed:
-		is_mouse_pressed = true
-		# Get the mouse position
-		var mouse_position = get_global_mouse_position()
-
-		# Check for double click
-		var current_time = OS.get_ticks_msec() / 1000.0
-		if current_time - last_click_time < double_click_delay:
-			# Double click detected, make the character jump
+	# if state == State.JUMPING:
+	# 	if Input.is_action_pressed('ui_left'):
+	# 		velocity.x = -move_speed
+	# 		sprite.flip_h = true
+	# 	elif Input.is_action_pressed('ui_right'):
+	# 		velocity.x = move_speed
+	# 		sprite.flip_h = false
+	if state == State.RUNNING:
+		if Input.is_action_just_pressed('ui_up') and is_on_floor():
 			velocity.y = -jump_strength
 			animation_player.play("jump")
-			print("jump")
+			state = State.JUMPING
 		else:
-			# Single click detected, move the character left or right
-			if mouse_position.x < global_position.x:
-				# Clicked on the left side of the screen
+			if Input.is_action_pressed('ui_left'):
+				# UI left command detected, move the character to the left
 				velocity.x = -move_speed
 				sprite.flip_h = true
-			else:
-				# Clicked on the right side of the screen
+				state = State.RUNNING
+				animation_player.play("run")
+			elif Input.is_action_pressed('ui_right'):
+				# UI right command detected, move the character to the right
 				velocity.x = move_speed
 				sprite.flip_h = false
+				state = State.RUNNING
+				animation_player.play("run")
+			else:
+				# No UI command detected, make the character sit
+				velocity.x *= friction
+				state = State.SITTING
+				# Do not play sit, wait until "run" is finised
+
+	if state == State.SITTING:
+		if Input.is_action_just_pressed('ui_up') and is_on_floor():
+			velocity.y = -jump_strength
+			animation_player.play("jump")
+			state = State.JUMPING
+
+		if Input.is_action_pressed('ui_left'):
+			# UI left command detected, move the character to the left
+			velocity.x = -move_speed
+			sprite.flip_h = true
+			state = State.RUNNING
 			animation_player.play("run")
+		elif Input.is_action_pressed('ui_right'):
+			# UI right command detected, move the character to the right
+			velocity.x = move_speed
+			sprite.flip_h = false
+			state = State.RUNNING
+			animation_player.play("run")
+		else:
+			# No UI command detected, make the character sit
+			velocity.x *= friction
+			state = State.SITTING
+			# Do not play sit, wait until "run" is finised
 
-		last_click_time = current_time
+	if state == State.JUMPING:
+		if Input.is_action_pressed('ui_left'):
+			velocity.x = -move_speed
+			sprite.flip_h = true
+		elif Input.is_action_pressed('ui_right'):
+			velocity.x = move_speed
+			sprite.flip_h = false
+		if is_on_floor():
+			state = State.SITTING
+			animation_player.play("sit")
 
-	if not Input.is_mouse_button_pressed(BUTTON_LEFT):
-		is_mouse_pressed = false
+	# Apply gravity
+	velocity.y += gravity * delta
 
 	# Move the KinematicBody2D
 	velocity = move_and_slide(velocity, Vector2.UP)
 
-	# Reset horizontal velocity if on floor
-	# if is_on_floor():
-	# 	velocity.x = 0
-	if is_on_floor() and not Input.is_mouse_button_pressed(BUTTON_LEFT):
-		animation_player.play("sit")
-
 func _on_VisibilityNotifier2D_screen_exited():
 	emit_signal("left_screen")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	# We do not loop the animation to make sure it finishes
+	# for smoother transition with the short runs
+	if state == State.SITTING:
+		animation_player.play("sit")
+	elif state == State.RUNNING:
+		animation_player.play("run")
